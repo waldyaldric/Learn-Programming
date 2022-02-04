f65a12a7ec9d4faaca8c94afbd6ed7e2844611de
waldy_aldric <96515993+waldyaldric@users.noreply.github.com>
2022-02-04 22:34:31 +0800

    Add files via upload

---
+++ Program_Biodata_waldy.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program_biodata_waldy;
+import java.util.Scanner;
+/**
+ *
+ * @author Asus
+ */
+public class Program_Biodata_waldy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nama : waldy pakatung");
+        System.out.println("Tempat lahir : Makassar");
+        System.out.println("Tanggal lahir : 14 September 2003");
+        System.out.println("Agama : Kristen Pentakosta");
+        System.out.println("Alamat : Jl. Gunung Merapi");
+        System.out.println("Nomor KTP : 1234567891011");
+        System.out.println("Nomor Telepon Rumah : 123456");
+        System.out.println("Nomor Handphone : 081943413211");
+        System.out.println("Program Studi : SIstem Informasi");
+        System.out.println("Fakultas : Fakultas Teknologi Informasi");
+        System.out.println("Universitas : Atmajaya Makassar");
+        System.out.println("Asal SMA : SMA  GAMALIEL Makassar");
+        System.out.println("Jurusan SMA : IPS");
+        System.out.println("Alasan Memilih Atmajaya : Saya memilih Kampus Atmajaya karena Atmajaya menghasilkan mahasiswa yang memiliki standar moral yang tinggi");
+    }   
+}      
