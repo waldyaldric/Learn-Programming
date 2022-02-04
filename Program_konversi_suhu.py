7adbfb32266739596d5cb29e2b2eb3c3d1268fe8
waldy_aldric <96515993+waldyaldric@users.noreply.github.com>
2022-02-04 22:53:11 +0800

    Add files via upload

---
+++ Program_konversi_suhu.py
@@ -0,0 +1,10 @@
+print("konversi suhu Clecius ke Fahrenheit, Reamur, dan Kelvin")
+c = int(input("Masukan nilai Celcius: "))
+f = c*9/5+32
+r = 4/5*c
+k = c + 273
+print ("Nilai suhu dalam Fahrenheit:", f)
+print ("Nilai suhu dalam Reamur:", r)
+print ("Nilai suhu dalam Kelvin:", k)
+print("")
+print("Akhir program")
